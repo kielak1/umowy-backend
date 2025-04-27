4e9e878191052d81bd78c012e2956274cdced80e
Tadeusz Kielak <tadeusz@kielak.com>
2025-04-27 19:23:11 +0000

    ngiinx

--- umowy/serializers.py
+++ umowy/serializers.py
@@ -6,16 +6,32 @@ class KontaktSerializer(serializers.ModelSerializer):
         model = Kontakt
         fields = '__all__'
 
-class KontrahentSerializer(serializers.ModelSerializer):
-    kontakt = KontaktSerializer()
+from rest_framework import serializers
+from .models import Umowa, Kontrahent
 
+class KontrahentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Kontrahent
-        fields = '__all__'
+        fields = ['id', 'nazwa_kontrahenta']
 
 class UmowaSerializer(serializers.ModelSerializer):
-    kontrahent = KontrahentSerializer()
+    kontrahent = KontrahentSerializer(read_only=True)
+    kontrahent_id = serializers.PrimaryKeyRelatedField(
+        queryset=Kontrahent.objects.all(),
+        source='kontrahent',
+        write_only=True
+    )
 
     class Meta:
         model = Umowa
-        fields = '__all__'
+        fields = [
+            'id',
+            'numer',
+            'przedmiot',
+            'data_zawarcia',
+            'czy_wymaga_kontynuacji',
+            'wymagana_data_zawarcia_kolejnej_umowy',
+            'czy_spelnia_wymagania_dora',
+            'kontrahent',
+            'kontrahent_id',
+        ]
